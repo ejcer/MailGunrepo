60c52afe230a67d4df5b449e5a6e27bd5ef849a3
emcenrue <edward.mcenrue@gmail.com>
2015-03-28 13:32:14 -0400

    testing quickstart

--- mailgun_api_user.py
+++ mailgun_api_user.py
@@ -8,6 +8,8 @@ def send_simple_message():
         "https://api.mailgun.net/v2/sandboxb92d03f6871d45f5912fbbec26d53347.mailgun.org/messages",
         auth=("api", "key-1c688c575c10981bd02726990837d3bd"),
         data={"from": "Excited User <mailgun@sandboxb92d03f6871d45f5912fbbec26d53347.mailgun.org>",
-              "to": ["bar@example.com", "edward.mcenrue@gmail.com"],
+              "to": ["edward.mcenrue@gmail.com", "edward.mcenrue@gmail.com"],
               "subject": "Hello",
               "text": "Testing some Mailgun awesomness!"})
+
+print(send_simple_message())
